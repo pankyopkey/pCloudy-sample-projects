1f153dba4279d78ff90003e4c260c134602e2a3c
Anshuman Chatterjee <anshuman.chatterji@gmail.com>
2018-02-07 11:33:41 +0530

    Refactored the sample projects. Added ReadMe.md

---
+++ DotNet/Appium dotNet Integrations/Appium Native Test/TestCase.cs
@@ -0,0 +1,91 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Remote;
+using ssts.util.pCloudy.AppiumAPIs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppiumAndroidExecution_dotNet
+{
+    class TestCase
+    {
+        private Uri appiumEndpoint;
+        private DesiredCapabilities capabilities;
+        private PCloudyAppiumSession pCloudyAppiumSession;
+
+
+        public TestCase(Uri appiumEndpoint, DesiredCapabilities capabilities, PCloudyAppiumSession pCloudyAppiumSession)
+        {
+            this.appiumEndpoint = appiumEndpoint;
+            this.capabilities = capabilities;
+            this.pCloudyAppiumSession = pCloudyAppiumSession;
+        }
+
+        public void run()
+        {
+            log("Creating the Android Driver Object");
+            try
+            {
+                AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(appiumEndpoint, capabilities);
+                log("Created the driver object");
+                //////////////////////////////////////////////
+                //////////////////////////////////////////////
+                //////////////////////////////////////////////
+                //////////////////////////////////////////////
+                //                                    ////////
+                //     Your TestScripts Goes Here     ////////
+                //                                    ////////
+                //////////////////////////////////////////////
+                //////////////////////////////////////////////
+                //////////////////////////////////////////////
+                //////////////////////////////////////////////
+
+
+                Keywords keywords = new Keywords(driver, this.pCloudyAppiumSession);
+                keywords.ObjectClick(By.Id("android:id/button1"), "Accept Button");
+                keywords.ObjectClick(By.Id("android:id/button1"), "OK Button");
+                keywords.ObjectClick(By.Id("com.ba.mobile:id/flightButton"), "BookFlight MenuItem");
+                keywords.ObjectClick(By.Id("com.ba.mobile:id/departure_airport"), "Combo Departure From");
+                keywords.typetext(By.Id("com.ba.mobile:id/search"), "Bengaluru");
+                keywords.ObjectClick(By.Id("com.ba.mobile:id/airport"), "CityName");
+                keywords.typetext(By.Id("com.ba.mobile:id/search"), "New Delhi");
+                keywords.ObjectClick(By.Id("com.ba.mobile:id/airport"), "CityName");
+                keywords.ObjectClick(By.Id("com.ba.mobile:id/singleTrip"), "Radio Button");
+                keywords.ObjectClick(By.Id("com.ba.mobile:id/depart_date_button"), "Departure DatePicker");
+                
+                
+                // Take Screenshot from Appium Driver
+                var screenshot = driver.GetScreenshot();
+                String fileName = "Screen-" + Guid.NewGuid() + ".png";
+                screenshot.SaveAsFile(fileName, System.Drawing.Imaging.ImageFormat.Png);
+
+                log("Appium Driver Screenshot is at: " + fileName);
+            }
+            catch (Exception ex)
+            {
+                log(ex.Message);
+            }
+            finally
+            {
+                // Take screenshot from pCloudy API 
+                var ss = pCloudyAppiumSession.takeScreenShot();
+                log("pCloudy API Screenshot is at: " + ss.FullName);
+
+                pCloudyAppiumSession.releaseSessionNow();
+                log("Released session after completion of Execution");
+            }
+
+
+        }
+
+
+        void log(string msg)
+        {
+            Console.Out.WriteLine("  "  + this.pCloudyAppiumSession.bookingDto.model.PadRight(20) + msg);
+        }
+
+    }
+}
--- DotNet/README.md
+++ DotNet/README.md
@@ -1,2 +1,19 @@
-# pCloudy-dotNet-Connector
-This will contain the .NEt connector as well as the sample projects
+
+# Appium dotNet Integrations
+As of now this contains only two sample projects:
+1- Appium Native Test
+## For automating Android Native Apps
+2- Appium Web Test
+## For both Chrome on Android as well as Safari of iOS.
+
+
+1- You may open the solution file "Appium pClouty Test.sln"
+2- Resolve the errors by providing your pCloudyEmail & pCloudyApiKey (you will get this from your Settings->API section in pCloudy website)
+3- Right click on the AppiumWeb or AppiumNative project and 'Set as Startup Project'
+4- Execute the project(or press F5)
+5- Visual Studio should first fetch the dependancies from Nuget and then start the execution
+6- Follow the instructions in the Console application to proceed
+
+#pCloudy-vb-connector
+Here the pCloudy-vb-connector is the project that is responsible to make REST calls to the pCloudy web apis and provide you a unified method of calling the apis as native .Net methods through code.
+This project is maintained by pCloudy Team. You may want to build this as-is.
