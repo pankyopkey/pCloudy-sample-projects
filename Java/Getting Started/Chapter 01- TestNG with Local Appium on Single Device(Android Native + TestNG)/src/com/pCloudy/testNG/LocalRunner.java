81a6407dcdd69e946ed1a6a175525d5d13854a89
Anshuman Chatterjee <anshuman.chatterjee@sstsinc.com>
2018-06-07 16:42:10 +0530

    Renamed Chapter 14

--- Java/Getting Started/Chapter 01- TestNG with Local Appium on Single Device(Android Native + TestNG)/src/com/pCloudy/testNG/LocalRunner.java
+++ Java/Getting Started/Chapter 01- TestNG with Local Appium on Single Device(Android Native + TestNG)/src/com/pCloudy/testNG/LocalRunner.java
@@ -1,6 +1,7 @@
 package com.pCloudy.testNG;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.openqa.selenium.By;
@@ -18,56 +19,58 @@ import io.appium.java_client.service.local.AppiumDriverLocalService;
 
 public class LocalRunner {
 
-	AppiumDriverLocalService service;
+	//AppiumDriverLocalService service;
 	AppiumDriver<WebElement> driver;
 
 	@BeforeTest
 	public void setUpSuite() throws Exception {
-		
-		//You may start  Appium server by below code or start manually
-//		 service = AppiumDriverLocalService.buildDefaultService();
-//		 service.start();
+
+		// You may start Appium server by below code or start manually
+		// service = AppiumDriverLocalService.buildDefaultService();
+		// service.start();
 	}
 
 	@BeforeMethod
 	public void prepareTest() throws IOException, InterruptedException {
+
 		
+	}
+
+	@Test
+	public void Test1() throws MalformedURLException {
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		capabilities.setCapability("newCommandTimeout", 600);
 		capabilities.setCapability("launchTimeout", 90000);
-		capabilities.setCapability("deviceName", Your_Android_Device_ID);
+		capabilities.setCapability("deviceName", "Your_Android_Device_ID");
 		capabilities.setCapability("browserName", "Android");
 		capabilities.setCapability("platformName", "Android");
 		capabilities.setCapability("appPackage", "com.pcloudy.appiumdemo");
 		capabilities.setCapability("appActivity", "com.ba.mobile.LaunchActivity");
 		capabilities.setCapability("rotatable", true);
-		capabilities.setCapability("app", System.getProperty("user.dir")+"/pCloudy Appium Demo.apk");
+		capabilities.setCapability("app", System.getProperty("user.dir") + "/pCloudy Appium Demo.apk");
+
+		driver = new AndroidDriver<WebElement>(new URL("http://127.0.0.1:1234/wd/hub"), capabilities);
 		
-		driver = new AndroidDriver<WebElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-	}
-
-	@Test
-	public void Test1() {
-
 		driver.findElement(By.xpath("//android.widget.Button[@resource-id='com.pcloudy.appiumdemo:id/accept' and @text='Accept']")).click();
 	}
 
-	@Test
-	public void Test2() {
-
-		driver.findElement(By.xpath("//android.widget.Button[@resource-id='com.pcloudy.appiumdemo:id/btnrefuse' and @text='Refuse']")).click();
-	}
+	// @Test
+	// public void Test2() {
+	//
+	// driver.findElement(By.xpath("//android.widget.Button[@resource-id='com.pcloudy.appiumdemo:id/btnrefuse' and @text='Refuse']")).click();
+	// }
 
 	@AfterMethod
-	public void endTest() throws  IOException {
+	public void endTest() throws IOException {
 
-		driver.quit();
+		if (driver != null)
+			driver.quit();
 	}
 
 	@AfterTest
 	public void tearDownSuite() throws IOException {
 
-		//Appium stop in AfterTest
-	    //service.stop();
+		// Appium stop in AfterTest
+		// service.stop();
 	}
 }
