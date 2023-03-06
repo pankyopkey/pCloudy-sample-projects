2e2fbf866f035c65146ad33170d5d28cda4e4a34
Advinsteve <abhinav.purokait@sstsinc.com>
2023-03-06 13:05:42 +0530

    Updated according to the new changes

---
+++ WDIO/Chapter2-WDIO+iOSOnSingleDevice/test/specs/testCaseios.js
@@ -0,0 +1,19 @@
+    var path = import('path');
+
+    describe('Demo App Testing',  () => {
+        
+      it('Login Functionality', async ()=> {
+
+        const Email = await $('//XCUIElementTypeApplication[1]/XCUIElementTypeWindow[1]/XCUIElementTypeOther[1]/XCUIElementTypeTextField[1]');
+        await Email.setValue('test@testname.com');
+        console.log("Email id is entered");
+        const password = await $('//XCUIElementTypeApplication[1]/XCUIElementTypeWindow[1]/XCUIElementTypeOther[1]/XCUIElementTypeSecureTextField[1]');
+        await password.setValue('testmunk');
+        console.log("password is entered");
+        const LButton = await $('//XCUIElementTypeApplication[1]/XCUIElementTypeWindow[1]/XCUIElementTypeOther[1]/XCUIElementTypeButton[1]');
+        await LButton.click();
+        console.log("login button is clicked");
+      });         
+    });
+
+
