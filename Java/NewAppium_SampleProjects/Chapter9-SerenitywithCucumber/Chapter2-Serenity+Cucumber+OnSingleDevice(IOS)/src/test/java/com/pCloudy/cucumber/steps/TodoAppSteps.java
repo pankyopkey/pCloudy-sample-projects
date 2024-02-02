76bee618e51eb05ca0c648cca9a898681831f5a3
Advinsteve <abhinav.purokait@sstsinc.com>
2024-02-03 04:17:55 +0530

    initial commit

---
+++ Java/NewAppium_SampleProjects/Chapter9-SerenitywithCucumber/Chapter2-Serenity+Cucumber+OnSingleDevice(IOS)/src/test/java/com/pCloudy/cucumber/steps/TodoAppSteps.java
@@ -0,0 +1,42 @@
+package com.pCloudy.cucumber.steps;
+
+import com.pCloudy.cucumber.pages.TodoApp;
+
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Given;
+
+public class TodoAppSteps {
+    TodoApp todo;
+    
+    @Given("^the user is on the login screen$")
+    public void the_user_is_on_the_login_screen() {
+    	System.out.println("App launched");
+    }
+
+
+    @When("^the user enters the email-id \"([^\"]*)\"$")
+    public void the_user_enters_the_email_id(String username) {
+    	todo.enterUsername(username);
+    	System.out.println("User Name Entered");
+    }
+
+    @When("^the user enters the password \"([^\"]*)\"$")
+    public void the_user_enters_the_password(String password) {
+    	todo.enterPassword(password);
+    	System.out.println("Password Entered");
+    }
+
+    @When("^clicks on the login button$")
+    public void clicks_on_the_login_button() {
+    	todo.clickLogin();
+    	System.out.println("Login CLicked");
+    }
+
+    @Then("^the user should be logged in successfully$")
+    public void the_user_should_be_logged_in_successfully() {
+    	System.out.println("App Login Checked");
+    }
+
+    
+}
---
+++ selenium/java/Chapter3-SerenitywithCucumber/Chapter1-Serenity+Cucumber+withpCloudyonSingleBrowser/src/test/java/com/pCloudy/cucumber/steps/TodoAppSteps.java
@@ -0,0 +1,36 @@
+package com.pCloudy.cucumber.steps;
+
+import com.pCloudy.cucumber.pages.TodoApp;
+
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+
+public class TodoAppSteps {
+    TodoApp todo;
+    
+    
+    @Given("^the user navigates to 'https://device\\.pcloudy\\.com/'$")
+    public void the_user_navigates_to_https_device_pcloudy_com() {
+    	todo.open();
+        System.out.println("Url open: '" + "https://device.pcloudy.com/");
+    }
+   
+	 @When("^the user enters the username and password$")
+	 public void the_user_enters_the_username_and_password(){
+		 todo.addNewElement();
+   }
+
+	 @When("^clicks on the login button$")
+	 public void clicks_on_the_login_button() {
+       todo.clickOn();
+       System.out.println("Login button Clicked");
+   }
+
+	 @Then("^the user should be logged in successfully$")
+	 public void the_user_should_be_logged_in_successfully(){
+		 System.out.println("Login checked successfully...");
+   }
+
+}
