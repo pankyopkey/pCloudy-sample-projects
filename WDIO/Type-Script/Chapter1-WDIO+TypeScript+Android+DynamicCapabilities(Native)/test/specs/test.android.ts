284157e1458857bc0baf6eaaba5f72950f924ef6
Advinsteve <abhinav.purokait@sstsinc.com>
2025-09-17 00:20:21 +0530

    feat:Language change script

--- WDIO/Type-Script/Chapter1-WDIO+TypeScript+Android+DynamicCapabilities(Native)/test/specs/test.android.ts
+++ WDIO/Type-Script/Chapter1-WDIO+TypeScript+Android+DynamicCapabilities(Native)/test/specs/test.android.ts
@@ -1,55 +1,54 @@
 import { $ } from '@wdio/globals'
 
-  describe('Demo App Testing',  () =>{
-      
-    it('Book A Flight Functionality-test', async ()=> {
-    
-      const acceptButton = await $('android=new UiSelector().resourceId("com.pcloudy.appiumdemo:id/accept")');
-        await acceptButton.waitForDisplayed({timeout : 30000});
-        await acceptButton.click();
-        console.log("accept button is clicked");
+describe('Start Acrobat', () => {
 
-      const flightButton = await $('android=new UiSelector().resourceId("com.pcloudy.appiumdemo:id/flightButton")');
-        await flightButton.waitForDisplayed({timeout: 30000});
-        await flightButton.click();
-        console.log("flight button is clicked");
+  it('should complete the Acrobat startup flow', async () => {
 
-      const fromLocation = await $('android=new UiSelector().resourceId("com.pcloudy.appiumdemo:id/spinnerfrom")');
-        await fromLocation.waitForDisplayed({timeout : 30000});
-        await fromLocation.click();
-        console.log("From is clicked");
-        const SelectFromCity = await $("//android.widget.CheckedTextView[@resource-id='android:id/text1' and @text='Bangalore, India (BLR)']");
-        await SelectFromCity.click();
-        console.log("from location is selected");
+       let close;
 
-      const toLocation = await $('android=new UiSelector().resourceId("com.pcloudy.appiumdemo:id/spinnerto")');
-        await toLocation.waitForDisplayed({timeout : 30000});
-        await toLocation.click();
-        console.log("To is clicked");
-        const SelecttoCity= await $("//android.widget.CheckedTextView[@resource-id='android:id/text1' and @text='Pune, India (PNQ)']");
-        await SelecttoCity.click();
-        console.log("To location is selected");
+    try {
+      // First locator (English)
+      close = await $('//android.widget.ImageView[@content-desc="Cross Button"]');
+      await close.waitForDisplayed({ timeout: 5000 });
+      await close.click();
+      console.log("Close Clicked (English)");
 
-      
-      const oneway = await $('android=new UiSelector().resourceId("com.pcloudy.appiumdemo:id/singleTrip")');
-        await oneway.waitForDisplayed({timeout : 30000});
-        await oneway.click();
-        console.log("oneway option is selected");
+    } catch (err) {
+      // Fallback locator (Russian)
+      close = await $('//android.widget.ImageView[@content-desc="Перекрестные кнопки"]');
+      await close.waitForDisplayed({ timeout: 5000 });
+      await close.click();
+      console.log("Close Clicked (Russian)");
+    }
 
-      const departureTime = await $('android=new UiSelector().resourceId("com.pcloudy.appiumdemo:id/txtdepart")');
-        await departureTime.waitForDisplayed({timeout : 30000});
-        await departureTime.click();
-        console.log("departureTime is selected");
-        const Okay= await $("//android.widget.Button[@resource-id='android:id/button1' and @text='OK']");
-        await Okay.click();
-        console.log("Ok is clicked");
+    // Continue button (first)
+    const continueButton1 = await $('//android.widget.Button[@resource-id="com.adobe.reader:id/continue_button"]');
+    await continueButton1.waitForDisplayed({ timeout: 10000 });
+    await continueButton1.click();
+    console.log("Continue Clicked");
 
-      const searchflightsButton = await $('android=new UiSelector().resourceId("com.pcloudy.appiumdemo:id/searchFlights")');
-        await searchflightsButton.waitForDisplayed({timeout : 30000});
-        await searchflightsButton.click();
-        console.log("searchflight button is clicked");
-      
-    });    
+    // Continue button (second)
+    const continueButton2 = await $('//android.widget.Button[@resource-id="com.adobe.reader:id/continue_button"]');
+    await continueButton2.waitForDisplayed({ timeout: 10000 });
+    await continueButton2.click();
+    console.log("Continue Clicked");
+
+    // Permission
+    const permission = await $('//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_deny_button"]');
+    await permission.waitForDisplayed({ timeout: 10000 });
+    await permission.click();
+    console.log("Permission Don't allow");
+
+    await browser.pause(2000);
+
+    // Close (again)
+    const close2 = await $('//android.widget.TextView[@resource-id="com.adobe.reader:id/fileName"]');
+    await close2.waitForDisplayed({ timeout: 10000 });
+    await close2.click();
+    console.log("File clicked");
+
+	 await browser.pause(5000);
+
   });
 
-
+});
